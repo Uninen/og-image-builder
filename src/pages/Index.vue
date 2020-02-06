cb5967498e738b3ddfd13b3c56f2c27fd45e532e
Ville Säävuori <ville@unessa.net>
2020-02-06 02:22:21 +0200

    Refactored app import to use ClientOnly-tag

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <Layout class="mb-8">
-    <!-- Replace everything inside this Layout with your own content -->
-    <!-- The main container CSS classes are in `src/layouts/Default.vue` -->
-    <builder-app v-if="loadApp"></builder-app>
+    <ClientOnly>
+      <builder-app></builder-app>
+    </ClientOnly>
   </Layout>
 </template>
 
@@ -20,16 +20,8 @@ export default {
     // We load this asynchronously so Gridsome wont try to render it build time
     BuilderApp: () => import('@/components/BuilderApp'),
   },
-  data: function() {
-    return {
-      loadApp: false,
-    }
-  },
   metaInfo: {
     title: 'Open Graph Image Builder',
-  },
-  mounted() {
-    this.loadApp = true
   },
 }
 </script>
